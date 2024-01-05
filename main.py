508c8306f4615cd0c210bf4113ab4f38b54a9466
Tyujhty <fannybaquey@gmail.com>
2024-01-05 14:50:31 +0000

    feat: change the logical of program. delete random function.

--- main.py
+++ main.py
@@ -3,32 +3,43 @@ from replit import clear
 from art import logo, vs
 from game_data import data
 
-def random_choice(data):
-    return random.choice(data)
-    
+
 def user_choice():
     return input("Who has more followers?: Type 'A' or 'B' ").upper()
 
-def compare(choice_a, choice_b, user_answer):
-    result_vs = ''
-    if choice_a['follower_count'] > choice_b['follower_count']:
-        result_vs = choice_a
-    else: 
-        result_vs = choice_b
-        
-    print(result_vs)
+def compare(computer_choices, score):
+    print(f"Compare A: {computer_choices[0]['name']}, a {computer_choices[0]['description']}, from {computer_choices[0]['country']}")
+    print(vs)
+    print(f"Against B: {computer_choices[1]['name']}, a {computer_choices[1]['description']}, from {computer_choices[1]['country']}\n")
+    user_guess = input("Who has more followers? Type 'A' or 'B': ").upper()
 
+    if computer_choices[0]['follower_count'] > computer_choices[1]['follower_count']:
+        result = 'A'
+        computer_choices[1] = random.choice(data)
+    else:
+        result = 'B'
+        computer_choices[0] = computer_choices[1]
+        computer_choices[1] = random.choice(data)
+
+    if user_guess == result:
+        score += 1
+        print(f"\nYou're right! Current score: {score}")
+        return True, score
+    else:
+        clear()
+        print(f"Sorry, you didn't guess. Your final score: {score}")
+        return False, score
+    
 def game():
+    keep_playing = True
     print(logo)
     print("Who has the more followers? Try to guess and defeate the computer\n")
-
-    choice_a = random_choice(data)
-    print(f"Compare A: {choice_a['name']}, a {choice_a['description']}, from {choice_a['country']}")
-    print(vs)
-    choice_b = random_choice(data)
-    print(f"Against B: {choice_b['name']}, a {choice_b['description']}, from {choice_b['country']}")
-
-    user_answer = user_choice()
-    print(user_answer)
-    compare(choice_a, choice_b, user_answer)
+    
+    computer_choices = []
+    '''Computer make a dictionary with 2 results'''
+    for _ in range(2):
+        computer_choices.append(random.choice(data))
+    score = 0
+    while keep_playing:
+        keep_playing, score = compare(computer_choices, score)
 game()
